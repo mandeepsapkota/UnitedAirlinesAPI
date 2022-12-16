5374dbf8fe58747d53caeab4ddae7b9ddbf26f8a
Mandeep Sapkota <mandeep.sapkota@gmail.com>
2022-12-16 00:04:41 -0500

    Add project files.

---
+++ Infrastructure/DependencyExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Models;
+using UnitedAirlinesAPI.Services;
+
+namespace UnitedAirlinesAPI.Infrastructure
+{
+    public static class DependencyExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            // Add swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "United Airlines API",
+                    Description = "Backend API for Alaska Airlines flight request",
+                });
+            });
+
+            //add configuration
+            services.Configure<SettingConfig>(configuration.GetSection("SettingConfig"));
+
+            //register services
+            services.AddScoped<ICargoService, CargoService>();
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/SettingConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace UnitedAirlinesAPI.Infrastructure
+{
+    public class SettingConfig
+    {
+        public string APIKey { get; set; }
+        public string MockUrl { get; set; }
+    }
+}
